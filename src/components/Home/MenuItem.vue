704acd314c4f65d8e4c2845a945c7f063ea7dcde
sulfikardi25 <sulfikardi25@gmail.com>
2020-07-30 16:25:45 +0700

    finish templating

--- src/components/Home/MenuItem.vue
+++ src/components/Home/MenuItem.vue
@@ -2,22 +2,20 @@
   <div class="content-menu">
     <div class="list-menu">
       <div class="item">
-
-      </div>
-      <div class="item">
-        
-      </div>
-      <div class="item">
-        
-      </div>
-      <div class="item">
-        
-      </div>
-      <div class="item">
-        
-      </div>
-      <div class="item">
-        
+        <div class="menu-img">
+          <img src="https://drive.google.com/uc?export=view&id=1ulOvJmayp3O5fO9bVPG1P93bKZn1Yyxt" alt="">
+        </div>
+        <div class="menu-price">
+          <p>Rp.100.000</p>
+        </div>
+        <div class="menu-name">
+          <img src="https://drive.google.com/uc?export=view&id=1FZg3slUpomIHEBwoOAbIjJ35ju0O2ug8" alt="" width="28">
+          <p>Big Double Burger</p>
+        </div>
+        <div class="review">
+          <i class="far fa-eye">Review</i>
+          <i class="fas fa-star mr-1">Rating</i>
+        </div>
       </div>
     </div>
     <Pagination />
